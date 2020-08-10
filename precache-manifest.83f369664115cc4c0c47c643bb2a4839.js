c88352dca0faf3647f91493d0bb4b29a886fc19d
EggTronic <EggTronic@users.noreply.github.com>
2020-08-10 06:33:52 +0000

    deploy: Boshan-Tech/Smart-Shower-App@f5043719235a966c5796231c879eb1f90024e178

---
+++ precache-manifest.83f369664115cc4c0c47c643bb2a4839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a27fa3f014d1a98841b768e48c981b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6eb67d44df70f654303",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "ab064156e468dbf96f9e",
+    "url": "/static/css/main.37764e9b.chunk.css"
+  },
+  {
+    "revision": "e6eb67d44df70f654303",
+    "url": "/static/js/2.c74711e6.chunk.js"
+  },
+  {
+    "revision": "0150c7c1195a20fa9d8d679baf16770b",
+    "url": "/static/js/2.c74711e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab064156e468dbf96f9e",
+    "url": "/static/js/main.d384ece9.chunk.js"
+  },
+  {
+    "revision": "a8a060b96edd618c944e",
+    "url": "/static/js/runtime-main.3d87772a.js"
+  }
+]);
