4ad6b8ba59efb2d30f4aef98ddf973ec55461a40
EggTronic <EggTronic@users.noreply.github.com>
2020-09-22 07:23:08 +0000

    deploy: Boshan-Tech/Smart-Shower-App@50fa81ebcfd64efd7e493840485ee3d51067a4d0

---
+++ precache-manifest.8878ff457c2fb186243b5f79448bf770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7574cb60f934d3b3327ce67c7f7aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df439a1ef1d4bd56bc3b",
+    "url": "/static/css/2.1be0eb63.chunk.css"
+  },
+  {
+    "revision": "f5a6649f75cdc7c994b1",
+    "url": "/static/css/main.579b1b20.chunk.css"
+  },
+  {
+    "revision": "df439a1ef1d4bd56bc3b",
+    "url": "/static/js/2.3a9fe8a7.chunk.js"
+  },
+  {
+    "revision": "7879a8856e3369756ed4d8fce39b7691",
+    "url": "/static/js/2.3a9fe8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a6649f75cdc7c994b1",
+    "url": "/static/js/main.fea46fa3.chunk.js"
+  },
+  {
+    "revision": "a8a060b96edd618c944e",
+    "url": "/static/js/runtime-main.3d87772a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74d60983aab61cf5eea05fa35549acb7.js"
+  "/precache-manifest.8878ff457c2fb186243b5f79448bf770.js"
 );
 
 self.addEventListener('message', (event) => {
